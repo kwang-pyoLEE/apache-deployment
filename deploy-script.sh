59626746c4181d49f048f4c795d089f4729fd60d
lkp7658 <103034692+kwang-pyoLEE@users.noreply.github.com>
2026-02-19 00:56:35 +0900

    Fix newline issue in deploy-script.sh
    utf-8 update

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Pod 이름을 찾아서 내용을 바꾸는 스크립트
 POD_NAME=$(kubectl get pod -l app=apache-target -o jsonpath='{.items[0].metadata.name}')
-
 kubectl exec $POD_NAME -- bash -c "echo \"<html><head><meta charset='utf-8'></head><body><h1>자동화 완성! Git에서 가져온 스크립트로 배포 성공!</h1></body></html>\" > /var/www/html/index.html"
