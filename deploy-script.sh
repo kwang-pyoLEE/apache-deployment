018c9201f5270c74226dad5f06e2bcf1303bcd0e
lkp7658 <103034692+kwang-pyoLEE@users.noreply.github.com>
2026-02-22 20:00:16 +0900

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Pod 이름을 찾아서 내용을 바꾸는 스크립트
 POD_NAME=$(kubectl get pod -l app=apache-target -o jsonpath='{.items[0].metadata.name}')
-kubectl exec $POD_NAME -- bash -c "echo \"<html><head><meta charset='utf-8'></head><body><h1>자동화 완성! Git에서 가져온 스크립트로 다시 테스트 진행 고고!!!</h1></body></html>\" > /var/www/html/index.html"
+kubectl exec $POD_NAME -- bash -c "echo \"<html><head><meta charset='utf-8'></head><body><h1>자동화 완성! Poll SCM으로 무인 배포 중!</h1></body></html>\" > /var/www/html/index.html"
